6349648b7873b8880e5af33b1aa8c691eae7cd41
Sophie Zhang <sophie_zhang@brown.edu>
2023-09-08 15:07:24 -0400

    fix: fixed assertion for wire whip

--- testing/src/test/java/edu/brown/cs/student/main/SortDemoTest.java
+++ testing/src/test/java/edu/brown/cs/student/main/SortDemoTest.java
@@ -1,9 +1,16 @@
 package edu.brown.cs.student.main;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import org.junit.Test;
 
 /** Tests for Java's sort. FOR DEMOING. */
 public class SortDemoTest {
   @Test
-  public void test() {}
+  public void test() {
+  }
+
 }
