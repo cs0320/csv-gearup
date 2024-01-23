4739dd7a52880a775ba377dcf9f2253204dde3d0
jmaffa <90526195+jmaffa@users.noreply.github.com>
2024-01-23 15:58:00 -0500

    added so it can be packaged and runnable

--- mixer/src/test/java/edu/brown/cs/student/mixer/attachments/WireWhipTest.java
+++ mixer/src/test/java/edu/brown/cs/student/mixer/attachments/WireWhipTest.java
@@ -18,6 +18,6 @@ public class WireWhipTest {
   @Test
   public void test() {
     WireWhip wireWhip = new WireWhip();
-    assertEquals("wire whippin", wireWhip.mix().whip());
+    assertEquals("Whipping cream!", wireWhip.mix().whip());
   }
 }
